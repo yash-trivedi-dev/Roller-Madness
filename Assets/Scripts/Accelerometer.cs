3f450fa70c69bd8e467c8cea16f19a627e37433f
Yash Trivedi <50360904+yash-trivedi-dev@users.noreply.github.com>
2020-04-26 22:26:06 +0530

    Initial commit

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public bool isFlat = true;
+    private Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector3 acc = Input.acceleration;
+         rigid.AddForce(acc.x * speed, 0, acc.y * speed);
+    }
+}
